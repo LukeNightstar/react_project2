6d84d8284f160a423116d0039161f229c8284e90
Eclipse_ <nightsky5070@gmail.com>
2023-08-17 17:36:16 +0900

    react

---
+++ src/component/TodoList.js
@@ -0,0 +1,18 @@
+import "./TodoList.css";
+import TodoItem from "./TodoItem";
+
+const TodoList = ({todo}) => {
+    return (
+        <div className="TodoList">
+            <h4>Todo List</h4>
+            <input className="searchbar" placeholder="검색어를 입력하세요"/>
+            <div className={"list_wrapper"}>
+                {todo.map((it) => (
+                    <TodoItem key={it.id} {...it}/>
+                ))}
+            </div>
+        </div>
+    )
+};
+
+export default TodoList;
